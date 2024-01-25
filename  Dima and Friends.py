e09d12a3e3fbd03e1fb9c73363c77c4a4fb2a2c2
Henabakos <henogato9876@gmail.com>
2024-01-25 10:52:11 +0300

    pangram Codeforce

---  Dima and Friends.py
+++  Dima and Friends.py
@@ -5,6 +5,5 @@ a=sum1%(num_of_dimasFreinds+1)
 cnt=0
 for i in range(sum1+1,sum1+6):
     if i%(num_of_dimasFreinds+1)!=1:
-        cnt+=1
-    
-print(cnt)               
+        cnt+=1  
+print(cnt)           
