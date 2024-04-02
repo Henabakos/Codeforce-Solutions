b24dc170ff2a78dadd488c998cd8617b8d36d577
Henabakos <henogato9876@gmail.com>
2024-04-02 07:14:06 -0900

    new push

--- ty.py
+++ ty.py
@@ -1,22 +1,3 @@
-# from collections import deque
-# n=int(input())
-# str1=input()
-# deq=deque()
-
-# if len(str1) % 2!=0:
-#     for i in range(len(str1)):
-#         if i%2!=0:
-#             deq.appendleft(str1[i])
-#         else:
-#             deq.append(str1[i])
-# else:
-#     for i in range(len(str1)):
-#         if i%2!=0:
-#             deq.append(str1[i])
-#         else:
-#             deq.appendleft(str1[i])
-        
-# print("".join(deq))
 
 # t=int(input())
 # for _ in range(t):
